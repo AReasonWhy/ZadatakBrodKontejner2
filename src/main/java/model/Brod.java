1e9114e248e139b92e69885704732f80e8b26d57
AReasonWhy <pharotek.jesod@gmail.com>
2017-01-20 18:52:56 +0100

    Uradjena klasa Brod closes #3

---
+++ src/main/java/model/Brod.java
@@ -0,0 +1,62 @@
+package model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Created by androiddevelopment on 20.1.17..
+ */
+@DatabaseTable
+public class Brod {
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_OZNAKA,canBeNull = false)
+    private String oznaka;
+    @DatabaseField(columnName = POLJE_NAZIV,canBeNull = false)
+    private String naziv;
+
+    public static final String POLJE_OZNAKA:"oznaka";
+    public static final String POLJE_NAZIV:"naziv";
+
+    public Brod(){
+
+    }
+
+    public Brod(String naziv, String oznaka){
+        this.naziv = naziv;
+        this.oznaka = oznaka;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    @Override
+    public String toString(){
+        return "Brod{" +
+                "id=" + id +
+                ", naziv='" + naziv + '\'' +
+                ", sa oznakom='" + oznaka + '\'' +
+                '}';
+    }
+}
