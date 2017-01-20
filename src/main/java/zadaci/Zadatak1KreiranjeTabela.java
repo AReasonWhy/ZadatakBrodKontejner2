9477ed16e4c8af8db65821a3c689d7ac2e8a0824
AReasonWhy <pharotek.jesod@gmail.com>
2017-01-20 18:40:34 +0100

    Kreirane klase za zadatke closes #2

---
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -0,0 +1,23 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+/**
+ * Created by androiddevelopment on 20.1.17..
+ */
+public class Zadatak1KreiranjeTabela {
+    public static void main(String[] args){
+        ConnectionSource connectionSource = null;
+        try {
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+
+
+        } catch ( Exception e ){
+            System.out.println("Greska");
+        }
+
+    }
+}
